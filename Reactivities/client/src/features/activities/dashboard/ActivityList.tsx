1be96c5056edfec057f8614866f52741ed889448
f.akbulut <f.akbulut@chainpoint.com>
2025-06-15 12:17:13 +0200

    End of Section 5

---
+++ Reactivities/client/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+import ActivityCard from "./ActivityCard";
+
+// Define properties for this component
+type Props = {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+}
+
+// Show each activity in a card
+// rendering a list of activity titles (using map)
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+  return (
+    <Box sx={{display: 'flex', flexDirection: 'column', gap: 3}}>
+        {activities.map(activity => (
+            <ActivityCard 
+              key={activity.id} 
+              activity={activity} 
+              selectActivity={selectActivity}
+              deleteActivity={deleteActivity}
+            />
+        ))}
+    </Box>
+  )
+}
